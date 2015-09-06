f127caa380d9200547b43d1ba2efc2f5a2b4602d
René Kraneis <rene.kraneis@gmail.com>
2015-09-06 20:30:21 +0200

    set up mvn clojure:repl

---
+++ clojure/repl.clj
@@ -0,0 +1,5 @@
+(ns user
+  (:require [de.rkraneis.projecteuler.clojure.solutions :refer :all]
+            [de.rkraneis.projecteuler.clojure.util :refer :all]
+            [clojure.tools.namespace.repl :refer [refresh set-refresh-dirs]]))
+(set-refresh-dirs "src/main/clojure")
