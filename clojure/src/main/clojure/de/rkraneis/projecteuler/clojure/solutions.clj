dd0f1637a90f94f89bcecdc3de58895b2204da4e
René Kraneis <rene.kraneis@gmail.com>
2015-09-06 20:30:47 +0200

    enable warnings for boxed math (clojure)

--- clojure/src/main/clojure/de/rkraneis/projecteuler/clojure/solutions.clj
+++ clojure/src/main/clojure/de/rkraneis/projecteuler/clojure/solutions.clj
@@ -1,5 +1,6 @@
 (ns de.rkraneis.projecteuler.clojure.solutions
   (:require [de.rkraneis.projecteuler.clojure.util :refer :all]))
+(set! *unchecked-math* :warn-on-boxed)
 
 (defn problem1-generated
   "Find the sum of all the multiples of 3 or 5 below 1000."
@@ -22,7 +23,7 @@
 
 (defn problem2 
   "By considering the terms in the Fibonacci sequence whose values do not
-   exceed four million, find the sum of the even-valued terms."
+  exceed four million, find the sum of the even-valued terms."
   [n]
   (reduce + (take-while
     (partial >= n)
@@ -40,20 +41,20 @@
 
 (defn problem5
   "What is the smallest positive number that is evenly divisable by all of the
-   numbers from 1 to 20?"
+  numbers from 1 to 20?"
   [] (comment "code a 'least common divisor' function and use that instead of pencil and paper")
   (* 2 2 2 2 3 3 5 7 11 13 17 19))
 
 (defn problem6
   "Find the difference between the sum of the squares of the first one hundred 
-   natural numbers and the square of the sum."
+  natural numbers and the square of the sum."
   [n]
   (- (#(* % %) (reduce + (range 1 (+ n 1))))
      (reduce + (map #(* % %) (range 1 (+ n 1))))))
 
 (defn problem7
   "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see 
-   that the 6th prime is 13.
-   What is the 10 001st prime number?"
+  that the 6th prime is 13.
+  What is the 10 001st prime number?"
   [n] (comment "lazyly evaluate all numbers and take the 10 001st")
   (last (take n (filter #(not (nil? %)) (map prime? (range))))))
